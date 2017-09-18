ae6f29a8d3f697a18741e81cf0ae48aa57ec6852
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-09-18 16:33:53 +0300

    Add Bind class to encapsulate ProxyFactory and update event

--- client/js/app/controllers/NegotiationController.js
+++ client/js/app/controllers/NegotiationController.js
@@ -1,58 +1,34 @@
 class NegotiationController {
   constructor() {
+
     let $ = document.querySelector.bind(document);
 
     this._inputDate = $("#date");
     this._inputQuantity = $("#quantity");
     this._inputValue = $("#value");
 
-    let self = this;
-    this._negotiationList = new Proxy(new NegotiationList(), {
-      get(target, prop, receiver) {
-        if (
-          ["add", "emptyList"].includes(prop) &&
-          typeof target[prop] == typeof Function
-        ) {
-          return function() {
-            console.log(`intercepting ${prop}`);
-            Reflect.apply(target[prop], target, arguments);
-            self._negotiationsView.update(target);
-          };
-        }
+    this._negotiationList = new Bind(
+      new NegotiationList(),
+      new NegotiationsView($("#negotiationsView")),
+      'add', 'emptyList');
 
-        return Reflect.get(target, prop, receiver);
-      }
-    });
+    this._message = new Bind(
+      new Message(),
+      new MessageView($("#messageView")),
+      'text');
 
-    // this._negotiationList = new NegotiationList(model =>
-    //   this._negotiationsView.update(model)
-    // );
-
-    this._negotiationsView = new NegotiationsView($("#negotiationsView"));
-    this._negotiationsView.update(this._negotiationList);
-    this._message = new Message();
-    this._messageView = new MessageView($("#messageView"));
-
-    this._messageView.update(this._message);
   }
 
   add(event) {
     event.preventDefault();
     this._negotiationList.add(this._createNegotiation());
-    this._negotiationsView.update(this._negotiationList);
-
     this._message.text = "Negotiation Added";
-    this._messageView.update(this._message);
-
     this._cleanForm();
   }
 
   delete() {
     this._negotiationList.emptyList();
-    this._negotiationsView.update(this._negotiationList);
-
     this._message.text = "Negotiations were deleted";
-    this._messageView.update(this._message);
   }
 
   _createNegotiation() {
