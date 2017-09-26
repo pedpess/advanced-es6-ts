12cefe0aa002a312c225a373579c2fd1591b0aa1
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-09-26 14:33:21 +0300

    Change server port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var http = require('http')
     ,app = require('./config/express');
 
-http.createServer(app).listen(3000, function() {
+http.createServer(app).listen(8080, function() {
     console.log('Server listening to the port: ' + this.address().port);
 });
 
